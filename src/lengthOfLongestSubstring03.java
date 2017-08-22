b8a6e7afcd8152f18939270b1534bf920ad42f8e
Shaowei <zsw22@vip.qq.com>
2017-08-22 16:27:11 +1000

    update Q3

--- src/lengthOfLongestSubstring03.java
+++ src/lengthOfLongestSubstring03.java
@@ -1,5 +1,28 @@
 class Solution {
-    public int lengthOfLongestSubstring(String s) {
-
+    public static int lengthOfLongestSubstring(String s) {
+        String[] sArray = s.split("");
+        String compareString = "";
+        int len = 0;
+        int compLen = 0;
+        for(int i = 0 ; i < sArray.length; i++) {
+            if (!compareString.contains(sArray[i])) {
+                compareString += sArray[i];
+                len++;
+            } else {
+                if (compLen < compareString.length()) {
+                    compLen = compareString.length();
+                }
+                compareString = sArray[i];
+                len = compareString.length();
+            }
+            System.out.println("Len:" + len);
+            System.out.println("SubString" + " " + compareString);
+        }
+        if(compLen < len){
+            return len;
+        }else {
+            return compLen;
+        }
     }
 }
+
