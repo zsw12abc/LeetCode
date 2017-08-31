302dde8229a8b55ddcffc6623059fadf7fc8e99f
Shaowei <zsw22@vip.qq.com>
2017-08-31 19:15:36 +1000

    add Q14 15 16

---
+++ src/threeSum.java
@@ -0,0 +1,37 @@
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+
+//Given an array S of n integers, are there elements a, b, c in S such that a + b + c = 0? Find all unique triplets in the array which gives the sum of zero.
+public class threeSum {
+    public List<List<Integer>> threeSum(int[] nums) {
+        List<List<Integer>> result = new ArrayList<>();
+        Arrays.sort(nums);
+        for (int i = 0; i < nums.length; i++){
+            if( i > 0 && nums[i] == nums[i-1]){
+                continue;
+            }
+            int j = i + 1;
+            int k = nums.length - 1;
+            int target = nums[i] * -1;
+            while(j < k){
+                if(nums[j] + nums[k] == target){
+                    result.add(Arrays.asList(nums[i], nums[j], nums[k]));
+                    j++;
+                    k--;
+                    while (j < k && nums[j] == nums[j - 1]){
+                        j++;
+                    }
+                    while(j < k && nums[k] == nums[k + 1]){
+                        k--;
+                    }
+                }else if (nums[j] + nums[k] > target){
+                    k--;
+                }else{
+                    j++;
+                }
+            }
+        }
+        return result;
+    }
+}
