70e14b16faa609f01b2acd9abd76dd9e82da3738
Shaowei <zsw22@vip.qq.com>
2017-09-05 13:50:07 +1000

    add Solution.java as Main class import the array into 3SumClosest.java

---
+++ src/Solution.java
@@ -0,0 +1,9 @@
+import java.util.List;
+
+public class Solution {
+    public static void main(String[] args) {
+        letterCombinations l = new letterCombinations();
+        List<String> result = l.letterCombinations("23");
+        System.out.println(result);
+    }
+}
