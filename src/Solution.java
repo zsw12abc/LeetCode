2d8ace5b8fadb5a0a6416087b13978b33d95e7f4
Shaowei <zsw22@vip.qq.com>
2017-09-05 16:46:57 +1000

    add Q18 4Sum

--- src/Solution.java
+++ src/Solution.java
@@ -1,9 +1,12 @@
+import java.util.Arrays;
 import java.util.List;
 
 public class Solution {
     public static void main(String[] args) {
-        letterCombinations l = new letterCombinations();
-        List<String> result = l.letterCombinations("23");
-        System.out.println(result);
+        fourSum f = new fourSum();
+        int[] nums = {-1,0,1,2,-1,-4};
+        for (List<Integer> integers : f.fourSum(nums, -1)) {
+            System.out.println(integers);
+        }
     }
 }
---
+++ src/fourSum.java
@@ -0,0 +1,39 @@
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+
+//Given an array S of n integers, are there elements a, b, c, and d in S such that a + b + c + d = target?
+//Find all unique quadruplets in the array which gives the sum of target.
+public class fourSum {
+    public List<List<Integer>> fourSum(int[] nums, int target) {
+        List<List<Integer>> result = new ArrayList<>();
+        Arrays.sort(nums);
+        for (int i = 0; i < nums.length; i++) {
+            for (int j = i + 1; j < nums.length; j++) {
+                int k = j + 1;
+                int end = nums.length - 1;
+                while (k < end) {
+                    if (nums[i] + nums[j] + nums[k] + nums[end] == target) {
+                        List<Integer> goChat = Arrays.asList(nums[i], nums[j], nums[k], nums[end]);
+                        if (!result.contains(goChat)) {// delete the duplicates: nums[j++] == nums[j]
+                            result.add(goChat);
+                        }
+                        k++;
+                        end--;
+                        while (k < end && nums[k] == nums[k - 1]) {
+                            k++;
+                        }
+                        while (k < end && nums[end] == nums[end + 1]) {
+                            end--;
+                        }
+                    } else if (nums[i] + nums[j] + nums[k] + nums[end] > target) {
+                        end--;
+                    } else {
+                        k++;
+                    }
+                }
+            }
+        }
+        return result;
+    }
+}
