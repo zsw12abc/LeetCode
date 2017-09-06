f33e73e1b268a8bda93d5eacefbc25bc6630f5b4
Shaowei <zsw22@vip.qq.com>
2017-09-06 20:33:30 +1000

    ADD Q24

---
+++ src/swapPairs.java
@@ -0,0 +1,26 @@
+public class swapPairs {
+    /**
+     * Given a linked list, swap every two adjacent nodes and return its head.
+     * For example,
+     * Given 1->2->3->4, you should return the list as 2->1->4->3.
+     * Your algorithm should use only constant space. You may not modify the values in the list, only nodes itself can be changed.
+     * https://leetcode.com/problems/swap-nodes-in-pairs/description/
+     *
+     * @param head
+     * @return
+     */
+    public ListNode swapPairs(ListNode head) {
+        ListNode dummy = new ListNode(0);
+        dummy.next = head;
+        ListNode current = dummy;
+        while (current.next != null && current.next.next != null) {
+            ListNode first = current.next;
+            ListNode second = current.next.next;
+            first.next = second.next;
+            current.next = second;
+            current.next.next = first;
+            current = current.next.next;
+        }
+        return dummy.next;
+    }
+}
