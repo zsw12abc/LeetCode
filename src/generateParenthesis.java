af405676dc7fd4699302780edae5244f46cca75e
Shaowei <zsw22@vip.qq.com>
2017-09-06 13:55:10 +1000

    UPDATE Q22

--- src/generateParenthesis.java
+++ src/generateParenthesis.java
@@ -1,3 +1,4 @@
+import java.util.ArrayList;
 import java.util.List;
 
 public class generateParenthesis {
@@ -6,9 +7,34 @@ public class generateParenthesis {
      * https://leetcode.com/problems/generate-parentheses/description/
      *
      * @param n
-     * @return
+     * @return all combinations in a list
      */
     public List<String> generateParenthesis(int n) {
+        List<String> list = new ArrayList<>();
+        backtrack(list, "", 0, 0, n);
+        return list;
+    }
 
+    /**
+     * The idea here is to only add '(' and ')' that we know will guarantee us a solution (instead of adding 1 too many close).
+     * Once we add a '(' we will then discard it and try a ')' which can only close a valid '('. Each of these steps are recursively called.
+     *
+     * @param list
+     * @param str
+     * @param open
+     * @param close
+     * @param max
+     */
+    private void backtrack(List<String> list, String str, int open, int close, int max) {
+        if (str.length() == max * 2) {
+            list.add(str);
+            return;
+        }
+        if (open < max) {
+            backtrack(list, str + "(", open + 1, close, max);
+        }
+        if (close < open) {
+            backtrack(list, str + ")", open, close + 1, max);
+        }
     }
 }
