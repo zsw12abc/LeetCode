cb5f5e83452ddeab99a5069457bb920658e01d97
Shaowei <zsw22@vip.qq.com>
2017-09-07 13:07:02 +1000

    ADD Q25

---
+++ src/reverseKGroup.java
@@ -0,0 +1,38 @@
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Stack;
+
+public class reverseKGroup {
+    /**
+     * Given a linked list, reverse the nodes of a linked list k at a time and return its modified list.
+     * k is a positive integer and is less than or equal to the length of the linked list. If the number of nodes is not a multiple of k then left-out nodes in the end should remain as it is.
+     * You may not alter the values in the nodes, only nodes itself may be changed.
+     * Only constant memory is allowed.
+     * https://leetcode.com/problems/reverse-nodes-in-k-group/description/
+     *
+     * @param head
+     * @param k
+     * @return
+     */
+    public ListNode reverseKGroup(ListNode head, int k) {
+        ListNode curr = head;
+        int count = 0;
+        while (curr != null && count != k) { // find the k+1 node
+            curr = curr.next;
+            count++;
+        }
+        if (count == k) { // if k+1 node is found
+            curr = reverseKGroup(curr, k); // reverse list with k+1 node as head
+            // head - head-pointer to direct part,
+            // curr - head-pointer to reversed part;
+            while (count-- > 0) { // reverse current k-group:
+                ListNode tmp = head.next; // tmp - next head in direct part
+                head.next = curr; // preappending "direct" head to the reversed list
+                curr = head; // move head of reversed part to a new node
+                head = tmp; // move "direct" head to the next node in direct part
+            }
+            head = curr;
+        }
+        return head;
+    }
+}
