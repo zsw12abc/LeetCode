d729879c35e09c39324af432acb5e74e30b17ea5
Shaowei <zsw22@vip.qq.com>
2017-09-07 15:47:01 +1000

    ADD Q31

---
+++ src/nextPermutation.java
@@ -0,0 +1,39 @@
+public class nextPermutation {
+    /**
+     * Implement next permutation, which rearranges numbers into the lexicographically next greater permutation of numbers.
+     * If such arrangement is not possible, it must rearrange it as the lowest possible order (ie, sorted in ascending order).
+     * The replacement must be in-place, do not allocate extra memory.
+     * https://leetcode.com/problems/next-permutation/description/
+     *
+     * @param nums
+     */
+    public void nextPermutation(int[] nums) {
+        int i = nums.length - 2;
+        while (i >= 0 && nums[i + 1] <= nums[i]) {
+            i--;
+        }
+        if (i >= 0) {
+            int j = nums.length - 1;
+            while (j >= 0 && nums[j] <= nums[i]) {
+                j--;
+            }
+            swap(nums, i, j);
+        }
+        reverse(nums, i + 1);
+    }
+
+    private void reverse(int[] nums, int start) {
+        int i = start, j = nums.length - 1;
+        while (i < j) {
+            swap(nums, i, j);
+            i++;
+            j--;
+        }
+    }
+
+    private void swap(int[] nums, int i, int j) {
+        int temp = nums[i];
+        nums[i] = nums[j];
+        nums[j] = temp;
+    }
+}
