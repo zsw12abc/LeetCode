53b26dd895cd6b2772c1bbcab42fa1f65046c2e7
Shaowei <zsw22@vip.qq.com>
2017-11-15 14:18:46 +1100

    add JavaScript Version

---
+++ js/twoSum.js
@@ -0,0 +1,19 @@
+/**
+ * Given an array of integers, return indices of the two numbers such that they add up to a specific target.
+ * You may assume that each input would have exactly one solution, and you may not use the same element twice.
+ * @param {number[]} nums
+ * @param {number} target
+ * @return {number[]}
+ */
+var twoSum = function (nums, target) {
+    var dict = {};
+    for (var i = 0; i < nums.length; i++) {
+        var inp = nums[i];
+        var diff = target - inp;
+        if (diff in dict) {
+            return [dict[diff], i];
+        }
+        dict[inp] = i;
+    }
+    return null;
+};
