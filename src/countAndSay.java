2ee3c25ae19a7da14bb9692ec0ccd3807d574867
Shaowei <zsw22@vip.qq.com>
2017-11-16 15:28:21 +1100

    add Q38 in Java (unfinished)

---
+++ src/countAndSay.java
@@ -0,0 +1,12 @@
+public class countAndSay {
+    /**
+     * Given an integer n, generate the nth term of the count-and-say sequence.
+     * Note: Each term of the sequence of integers will be represented as a string.
+     *
+     * @param n
+     * @return
+     */
+    public String countAndSay(int n) {
+
+    }
+}
