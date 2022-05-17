c4257fa21308e514e385efebc315463bcabbe6df
Shaowei <11704698+zsw12abc@users.noreply.github.com>
2022-05-17 21:32:21 +1000

    add QueueNode

---
+++ BinaryTree/BasicClass/QueueNode.cs
@@ -0,0 +1,14 @@
+﻿namespace BinaryTree
+{
+    public class QueueNode
+    {
+        public TreeNode node;
+        public int depth;
+
+        public QueueNode(TreeNode node, int depth)
+        {
+            this.node = node;
+            this.depth = depth;
+        }
+    }
+}
