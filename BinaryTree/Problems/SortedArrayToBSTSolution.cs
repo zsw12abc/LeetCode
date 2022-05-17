3436e078fac8aa0e217e830d7df754f0ed69dfea
Shaowei <11704698+zsw12abc@users.noreply.github.com>
2022-05-17 19:12:57 +1000

    LeetCode 108

---
+++ BinaryTree/Problems/SortedArrayToBSTSolution.cs
@@ -0,0 +1,28 @@
+﻿using System.Linq;
+
+namespace BinaryTree
+{
+    /// <summary>
+    /// 108. 将有序数组转换为二叉搜索树
+    /// 给你一个整数数组 nums ，其中元素已经按 升序 排列，请你将其转换为一棵 高度平衡 二叉搜索树。
+    /// 高度平衡 二叉树是一棵满足「每个节点的左右两个子树的高度差的绝对值不超过 1 」的二叉树。
+    /// </summary>
+    public class SortedArrayToBSTSolution
+    {
+        public TreeNode SortedArrayToBST(int[] nums)
+        {
+            var len = nums.Length;
+            var midIndex = nums.Length / 2;
+            if (nums.Length == 0)
+            {
+                return null;
+            }
+
+            return new TreeNode(nums[midIndex])
+            {
+                left = SortedArrayToBST(nums.Where((val, index) => index < midIndex).ToArray()),
+                right = SortedArrayToBST(nums.Where((val, index) => index > midIndex).ToArray())
+            };
+        }
+    }
+}
