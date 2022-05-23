0b485b43c3d2388d5b4ed97ecf33ce928dd2aabb
Shaowei <11704698+zsw12abc@users.noreply.github.com>
2022-05-23 19:05:59 +1000

    Add project for Heap, LeetCode 23

---
+++ Heap/Problems/MergeKListsSolution.cs
@@ -0,0 +1,72 @@
+﻿using System;
+using System.Collections.Generic;
+using Heap.BasicClass;
+
+namespace Heap.Problems
+{
+    /// <summary>
+    /// 23. 合并K个升序链表
+    /// 给你一个链表数组，每个链表都已经按升序排列。
+    /// 请你将所有链表合并到一个升序链表中，返回合并后的链表。
+    /// </summary>
+    public class MergeKListsSolution
+    {
+        /// <summary>
+        /// 
+        /// </summary>
+        /// <param name="lists"></param>
+        /// <returns></returns>
+        public ListNode MergeKLists(ListNode[] lists)
+        {
+            return Merge(lists, 0, lists.Length - 1);
+        }
+
+        // 分治合并
+        private ListNode Merge(ListNode[] lists, int l, int r)
+        {
+            if (l == r)
+            {
+                return lists[l];
+            }
+
+            if (l > r)
+            {
+                return null;
+            }
+
+            var mid = (l + r) >> 1;
+            return MergeTwoLists(Merge(lists, l, mid), Merge(lists, mid + 1, r));
+        }
+
+        private ListNode MergeTwoLists(ListNode a, ListNode b)
+        {
+            if (a == null || b == null)
+            {
+                return a ?? b;
+            }
+
+            var head = new ListNode(0);
+            var tail = head;
+            var aPtr = a;
+            var bPtr = b;
+            while (aPtr != null && bPtr != null)
+            {
+                if (aPtr.val < bPtr.val)
+                {
+                    tail.next = aPtr;
+                    aPtr = aPtr.next;
+                }
+                else
+                {
+                    tail.next = bPtr;
+                    bPtr = bPtr.next;
+                }
+
+                tail = tail.next;
+            }
+
+            tail.next = (aPtr ?? bPtr);
+            return head.next;
+        }
+    }
+}
---
+++ Heap/Program.cs
@@ -0,0 +1,9 @@
+﻿namespace Heap
+{
+    internal class Program
+    {
+        public static void Main(string[] args)
+        {
+        }
+    }
+}
