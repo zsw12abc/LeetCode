bcde35042068128229934f777d443eb39135e5cd
Shaowei <11704698+zsw12abc@users.noreply.github.com>
2022-05-25 18:48:03 +1000

    LeetCode 295 update to C#6.0

---
+++ Heap/Problems/MedianFinderSolution.cs
@@ -0,0 +1,59 @@
+﻿using System.Collections.Generic;
+using System.Runtime.InteropServices;
+
+
+namespace Heap.Problems
+{
+    /// <summary>
+    /// 295. 数据流的中位数
+    /// 中位数是有序列表中间的数。如果列表长度是偶数，中位数则是中间两个数的平均值。
+    /// 例如，[2,3,4]的中位数是 3
+    /// [2,3] 的中位数是 (2 + 3) / 2 = 2.5
+    /// 设计一个支持以下两种操作的数据结构：
+    /// void addNum(int num) - 从数据流中添加一个整数到数据结构中。
+    /// double findMedian() - 返回目前所有元素的中位数。
+    /// </summary>
+    public class MedianFinderSolution
+    {
+        private PriorityQueue<int, int> queMin;
+        private PriorityQueue<int, int> queMax;
+
+        public MedianFinderSolution()
+        {
+            queMin = new PriorityQueue<int, int>(Comparer<int>.Create((x, y) => y - x));
+            queMax = new PriorityQueue<int, int>(Comparer<int>.Create((x, y) => x - y));
+        }
+
+        public void AddNum(int num)
+        {
+            if (queMin.Count == 0 || num <= queMin.Peek())
+            {
+                queMin.Enqueue(num, num);
+                if (queMax.Count + 1 < queMin.Count)
+                {
+                    var el = queMin.Dequeue();
+                    queMax.Enqueue(el, el);
+                }
+            }
+            else
+            {
+                queMax.Enqueue(num, num);
+                if (queMax.Count > queMin.Count)
+                {
+                    var el = queMax.Dequeue();
+                    queMin.Enqueue(el, el);
+                }
+            }
+        }
+
+        public double FindMedian()
+        {
+            if (queMin.Count > queMax.Count)
+            {
+                return queMin.Peek();
+            }
+
+            return (queMin.Peek() + queMax.Peek()) / 2.0;
+        }
+    }
+}
--- Heap/Program.cs
+++ Heap/Program.cs
@@ -1,30 +1,32 @@
-﻿using System.Collections.Generic;
+﻿// See https://aka.ms/new-console-template for more information
+
+//23. 合并K个升序链表
+
+using System.Text.RegularExpressions;
 using Heap.BasicClass;
 using Heap.Problems;
 
-namespace Heap
-{
-    internal class Program
-    {
-        public static void Main(string[] args)
-        {
-            //23. 合并K个升序链表
-            var testCase23 = new ListNode[] { new ListNode(new List<int>() { 1, 4, 5 }), new ListNode(new List<int>() { 1, 3, 4 }), new ListNode(new List<int>() { 2, 6 }) };
-            var result23 = new MergeKListsSolution().MergeKLists(testCase23);
-            result23.DisplayListNode();
+var testCase23 = new ListNode[] { new ListNode(new List<int>() { 1, 4, 5 }), new ListNode(new List<int>() { 1, 3, 4 }), new ListNode(new List<int>() { 2, 6 }) };
+var result23 = new MergeKListsSolution().MergeKLists(testCase23);
+result23.DisplayListNode();
 
-            //215. 数组中的第K个最大元素
-            var testCase215Nums = new int[] { 3, 2, 1, 5, 6, 4 };
-            var testCase215K = 2;
-            var result215 = FindKthLargestSolution.FindKthLargest(testCase215Nums, testCase215K);
+//215. 数组中的第K个最大元素
+var testCase215Nums = new int[] { 3, 2, 1, 5, 6, 4 };
+var testCase215K = 2;
+var result215 = FindKthLargestSolution.FindKthLargest(testCase215Nums, testCase215K);
 
-            //218. 天际线问题
-            var testCase218 = new int[][] { new[] { 0, 3, 2 }, new[] { 1, 4, 3, }, new[] { 3, 5, 4 }, new[] { 5, 7, 3 } };
-            var result218 = GetSkylineSolution.GetSkyline(testCase218);
+//218. 天际线问题
+var testCase218 = new int[][] { new[] { 0, 3, 2 }, new[] { 1, 4, 3, }, new[] { 3, 5, 4 }, new[] { 5, 7, 3 } };
+var result218 = GetSkylineSolution.GetSkyline(testCase218);
 
-            //264. 丑数 II
-            var testCase264 = 19;
-            var result264 = NthUglyNumberSolution.NthUglyNumber(testCase264);
-        }
-    }
-}
+//264. 丑数 II
+var testCase264 = 19;
+var result264 = NthUglyNumberSolution.NthUglyNumber(testCase264);
+
+//295. 数据流的中位数
+var MedianFinder = new MedianFinderSolution();
+MedianFinder.AddNum(1);
+MedianFinder.AddNum(2);
+Console.WriteLine(MedianFinder.FindMedian());
+MedianFinder.AddNum(3);
+Console.WriteLine(MedianFinder.FindMedian());
