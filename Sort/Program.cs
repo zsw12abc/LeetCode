1c1ccc0b9e6029cad576ee6a4826982da77c666f
Shaowei <11704698+zsw12abc@users.noreply.github.com>
2022-08-09 19:41:34 +1000

    Add Sort

---
+++ Sort/Program.cs
@@ -0,0 +1,10 @@
+﻿using Sort.Problems;
+
+int[] arr = { 3, 38, 5, 44, 15, 36, 26, 27, 2, 48, 4, 19, 47, 50, 48 };
+var bubbleSort = new BubbleSort();
+var bubbleSortResult = bubbleSort.Sort(arr);
+Console.Out.WriteLine(string.Join(",", bubbleSortResult));
+
+var selectionSort = new SelectionSort();
+var selectionSortResult = selectionSort.Sort(arr);
+Console.Out.WriteLine(string.Join(",", selectionSortResult));
