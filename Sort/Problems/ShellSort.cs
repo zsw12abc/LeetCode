64e50ed6ca7b670b9c767966ae76c6e0730509f1
Shaowei <11704698+zsw12abc@users.noreply.github.com>
2022-08-10 18:22:16 +1000

    Update ShellSort.cs

---
+++ Sort/Problems/ShellSort.cs
@@ -0,0 +1,27 @@
+﻿namespace Sort.Problems;
+
+public class ShellSort
+{
+    public int[] Sort(int[] arr)
+    {
+        int length = arr.Length;
+        int temp;
+        for (var step = length / 2; step >= 1; step /= 2)
+        {
+            for (var i = step; i < length; i++)
+            {
+                temp = arr[i];
+                int j = i - step;
+                while (j >= 0 && arr[j] > temp)
+                {
+                    arr[j + step] = arr[j];
+                    j -= step;
+                }
+
+                arr[j + step] = temp;
+            }
+        }
+
+        return arr;
+    }
+}
--- Sort/Program.cs
+++ Sort/Program.cs
@@ -1,6 +1,6 @@
 ﻿using Sort.Problems;
 
-int[] arr = { 3, 38, 5, 44, 15, 36, 26, 27, 2, 48, 4, 19, 47, 50, 48 };
+int[] arr = { 3, 38, 5, 44, 15, 36, 26, 27, 2, 49, 4, 19, 47, 50, 48 };
 var bubbleSort = new BubbleSort();
 var bubbleSortResult = bubbleSort.Sort(arr);
 Console.Out.WriteLine(string.Join(",", bubbleSortResult));
@@ -8,3 +8,12 @@ Console.Out.WriteLine(string.Join(",", bubbleSortResult));
 var selectionSort = new SelectionSort();
 var selectionSortResult = selectionSort.Sort(arr);
 Console.Out.WriteLine(string.Join(",", selectionSortResult));
+
+
+var insertionSort = new InsertionSort();
+var insertionSortResult = insertionSort.Sort(arr);
+Console.Out.WriteLine(string.Join(",", insertionSortResult));
+
+var shellSort = new ShellSort();
+var shellSortResult = shellSort.Sort(arr);
+Console.Out.WriteLine(string.Join(",", shellSortResult));
